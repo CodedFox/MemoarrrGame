78281536df84510aea485f0d8fb43fb381fd5fe8
CodedFox <jonathan.gratton@gmail.com>
2018-11-15 10:22:32 -0500

    Player class completed in .h

--- include/player.h
+++ include/player.h
@@ -10,20 +10,43 @@ class Reward;
 // side of the board (top, bottom, left or right) and has the current count of rubies.
 class Player {
     private:
-        bool endOfGame;
+        std::string name;
+        std::string boardSide; // top, bottom, left, right
+        bool active = true;
+        int rubies = 0;
+        bool endOfGame = false;
 
     public:
-        std::string getName(); // const return the name of the player.
-        void setActive(bool); // set the status of the player as active or inactive.
-        bool isActive(); // returns true if the player is active.
-        int getNRubies(); // const return the number of rubies won by this player.
-        void addReward( const Reward& ); // add a reward with a given number of rubies.
-        void setDisplayMode(bool endOfGame);
+        Player() {};
+        Player(std::string playerName, std::string side): name(playerName), boardSide(side) {};
+        ~Player() {};
+        inline std::string getName() const { return name; }; // const return the name of the player.
+        inline void setActive(bool status) { active = status; }; // set the status of the player as active or inactive.
+        inline bool isActive() const { return active; }; // returns true if the player is active.
+        inline int getNRubies() const { return rubies; }; // const return the number of rubies won by this player.
+        inline void addReward( const Reward& ) { }; // add a reward with a given number of rubies.
+        inline void setDisplayMode(bool gameEnd) { endOfGame = gameEnd; };
 
-        // A player must be printable with the insertion operator cout << player. An example print out with endOfGame false could look as follows:
-        // Joe Remember Doe: left (active)
-        // Once endOfGame is true:
-        // Joe Remember Doe:  3 rubies
+        friend std::ostream& operator<<(std::ostream & os, const Player & player);
 };
+
+// A player must be printable with the insertion operator cout << player. An example print out with endOfGame false could look as follows:
+// Joe Remember Doe: left (active)
+// Once endOfGame is true:
+// Joe Remember Doe:  3 rubies
+std::ostream& operator<<(std::ostream & os, const Player & player) {
+    if (!player.endOfGame) {
+        os << player.name << ": " << player.boardSide;
+        
+        if (player.active) {
+            os << " (active)";
+        } else {
+            os << "inactive";
+        }
+    } else {
+        os << player.name << ": " << player.rubies << " rubies";
+    }
+    return os;
+}
 
 #endif
