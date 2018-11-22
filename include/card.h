0bc85c1552bcf57a8cf0a323895f3e26f81a77b4
CodedFox <jonathan.gratton@gmail.com>
2018-11-22 00:17:48 -0500

    Created card class

--- include/card.h
+++ include/card.h
@@ -1,23 +1,41 @@
 #ifndef CARD_H
 #define CARD_H
 
+#include <iostream>
+// #include <string>
+
     // Design a class Card which can take a face of one of the five possible animals and 
-    // one of the five background colours. A card must also be “printable” as one string 
-    // per row with the method:
+    // one of the five background colours. 
 class Card {
     private:
-        
+        enum class FaceAnimal {Crab, Penguin, Octopus, Turtle, Walrus};
+        enum class FaceBackground {Red, Green, Purple, Blue, Yellow};
+        // Note that Penguin and Red are enumeration values of type FaceAnimal and FaceBackground.
+
+        FaceAnimal animal;
+        FaceBackground background;
+
+        // An object of type Card can not be copied or assigned and needs a private constructor 
+        // but will give CardDeck (see below) friend access.
+        Card() {};
+        // Card c(Penguin,Red); // This constructor will be private
+        Card(FaceAnimal fa, FaceBackground fb): animal(fa), background(fb) {};
+        ~Card() {};
 
     public:
-        // Card c(Penguin,Red); // This constructor will be private
+        // A card must also be “printable” as one string per row with the method:
         // for (int row = 0; row <c.getNRows(); ++row ) {
         //      std::string rowString = c(row);
         //      std::cout << rowString << std::endl;
         // }
+        
+        inline FaceAnimal getAnimal() const { return animal; };
+        inline FaceBackground getBackground() const { return background; };
 
-        // Note that Penguin and Red are enumeration values of type FaceAnimal and FaceBackground.
-        // An object of type Card can not be copied or assigned and needs a private constructor 
-        // but will give CardDeck (see below) friend access.
+        friend std::ostream& operator<<(std::ostream & os, const FaceAnimal & fa);
+        friend std::ostream& operator<<(std::ostream & os, const FaceBackground & fb);
+        
+        std::string operator()(int row);
 };
 
 #endif
--- src/card.cpp
+++ src/card.cpp
@@ -1,4 +1,43 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 #include "card.h"
+
+std::ostream & operator<<(std::ostream & os, const Card::FaceAnimal & fa) {
+    switch(fa) {
+        case Card::FaceAnimal::Crab: os << "C"; break;
+        case Card::FaceAnimal::Penguin: os << "P"; break;
+        case Card::FaceAnimal::Octopus: os << "O"; break;
+        case Card::FaceAnimal::Turtle: os << "T"; break;
+        case Card::FaceAnimal::Walrus: os << "W"; break;
+        default: os << "Error - Face Animal"; break;
+    }
+    return os;
+}
+
+std::ostream & operator<<(std::ostream & os, const Card::FaceBackground & fb) {
+    switch(fb) {
+        case Card::FaceBackground::Red: os << "r"; break;
+        case Card::FaceBackground::Green: os << "g"; break;
+        case Card::FaceBackground::Purple: os << "p"; break;
+        case Card::FaceBackground::Blue: os << "b"; break;
+        case Card::FaceBackground::Yellow: os << "y"; break;
+        default: os << "Error - Face Background"; break;
+    }
+    return os;
+}
+
+std::string Card::operator()(int row) {
+    std::ostringstream oss;
+    if (row >= 0 && row < 3) {
+        if (row%2 == 1) {
+            oss << background << animal << background;
+        } else {
+            oss << background << background << background;
+        }
+    } else {
+        throw std::invalid_argument( "Invalid Row" );
+    }
+    return oss.str();
+}
