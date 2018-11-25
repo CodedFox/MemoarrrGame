02c63451cb8389d599a36fb64f6252dcf64eca7d
CodedFox <jonathan.gratton@gmail.com>
2018-11-25 00:13:01 -0500

    created CardDeck as singleton class

--- include/cardDeck.h
+++ include/cardDeck.h
@@ -1,11 +1,24 @@
 #ifndef CARD_DECK_H
 #define CARD_DECK_H
 
+#include <card.h>
+class Card;
+
+#include <deck.h>
+// template <> 
+// class Deck<Card>;
+
+
 // Design a class CardDeck derived from Deck<Card>.
-class CardDeck {
+// template <typename Card>
+class CardDeck: public Deck<Card> {
     private:
+        static CardDeck * cd;
 
     public:
+        CardDeck(): Deck<Card>() {};
+        // ~CardDeck() { delete cd; };
+
         static CardDeck& make_CardDeck(); // is the only public method for CardDeck. The method has to always return the same CardDeck during the execution of the program.
 
         // An object of type CardDeck can not be copied or assigned, and it has no public constructor.
