e60dd156257418f1a1cb9bc8b426b6b3d17be9f4
CodedFox <jonathan.gratton@gmail.com>
2018-11-25 00:13:13 -0500

    created RewardDeck as singleton class

--- include/rewardDeck.h
+++ include/rewardDeck.h
@@ -1,13 +1,23 @@
 #ifndef REWARD_DECK_H
 #define REWARD_DECK_H
 
+#include <reward.h>
+class Reward;
+
+#include <deck.h>
+// template <> 
+// class Deck<Reward>;
+
 // Design a class RewardDeck derived from Deck<Reward> with the corresponding properties to CardDeck.
-class RewardDeck {
+// template <typename Reward>
+class RewardDeck: public Deck<Reward> {
     private:
-        
+        static RewardDeck * rd;
 
     public:
-        
+        RewardDeck() {};
+        // ~RewardDeck() {};
+        static RewardDeck& make_RewardDeck();
 };
 
 #endif
--- src/rewardDeck.cpp
+++ src/rewardDeck.cpp
@@ -2,3 +2,23 @@
 #include <string>
 
 #include "rewardDeck.h"
+
+RewardDeck* RewardDeck::rd = nullptr;
+
+RewardDeck& RewardDeck::make_RewardDeck() {
+    if (!rd) {
+        rd = new RewardDeck();
+        (*rd).cards.push_back(Reward(1));
+        (*rd).cards.push_back(Reward(1));
+        (*rd).cards.push_back(Reward(1));
+        (*rd).cards.push_back(Reward(2));
+        (*rd).cards.push_back(Reward(2));
+        (*rd).cards.push_back(Reward(3));
+        (*rd).cards.push_back(Reward(4));
+        (*rd).shuffle();
+    } else {
+        throw std::runtime_error( "Reward Deck already created." );
+    }
+    
+    return *rd;
+}
