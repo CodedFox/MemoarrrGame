39a123a9423c246cc55a271a5d58467c9f53040f
CodedFox <jonathan.gratton@gmail.com>
2018-11-26 02:36:11 -0500

    completed base rules

--- include/rules.h
+++ include/rules.h
@@ -10,9 +10,10 @@ class Rules {
         
 
     public:
-        bool isValid(const Game&); // returns true is previous and current card match; false otherwise.
-        bool gameOver(const Game&); // returns true if the number of rounds has reached 7.
-        bool roundOver(const Game&); // returns true if there is only one active Player left.
+        bool isValid( const Game& ); // returns true is previous and current card match; false otherwise.
+        bool gameOver( const Game& ); // returns true if the number of rounds has reached 7.
+        bool roundOver( const Game& ); // returns true if there is only one active Player left.
+        inline const Player& getNextPlayer( const Game& g ) { return g.getCurrentPlayer()->second; };
 };
 
 #endif
--- src/rules.cpp
+++ src/rules.cpp
@@ -2,19 +2,34 @@
 #include <string>
 
 #include "rules.h"
-// #include "game.h"
+#include "game.h"
 
-bool Rules::isValid(const Game&) {
-
-    return true;
+// returns true is previous and current card match; false otherwise.
+bool Rules::isValid( const Game& g ) {
+    if (g.getCurrentCard()->getAnimal() == g.getPreviousCard()->getAnimal()) {
+        return true;
+    }
+    if (g.getCurrentCard()->getBackground() == g.getPreviousCard()->getBackground()) {
+        return true;
+    }
+    return false;
 }
 
-bool Rules::gameOver(const Game&) {
-
-    return true;
+// returns true if the number of rounds has reached 7.
+bool Rules::gameOver( const Game& g ) {
+    if (g.getRound() == 6) {
+        return true;
+    }
+    return false;
 }
 
-bool Rules::roundOver(const Game&) {
-
-    return true;
+// returns true if there is only one active Player left.
+bool Rules::roundOver( const Game& g ) {
+    int count = 0;
+    for (auto player : g.getPlayers()) {
+        if (player.second.isActive()) {
+            count++;
+        }
+    }
+    return (count == 1);
 }
