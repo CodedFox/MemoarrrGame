85c9f75f4c3bda40385cd281edc989bd99f88f87
CodedFox <jonathan.gratton@gmail.com>
2018-11-26 02:03:32 -0500

    completed game class

--- include/game.h
+++ include/game.h
@@ -1,27 +1,42 @@
 #ifndef GAME_H
 #define GAME_H
 
-// #include "player.h"
+#include <map>
+#include <utility>
+
+#include "player.h"
 class Player;
 
-// #include "card.h"
+#include "card.h"
 class Card;
+
+#include "board.h"
+class Board;
 
 // Design a class Game that encapsulates the current state of the game and it will have 
 // a class variable of type Board. It is responsible to print the current state of the game.
 class Game {
     private:
-
+        int round = 0;
+        std::map<Side, Player> players;
+        Board board;
+        Card * previousCard = nullptr;
+        Card * currentCard = nullptr;
 
     public:
-        int getRound(); // returns a number between 0 and 6 corresponding to the current round of the game
-        void addPlayer( const Player& ); // which adds a Player to this game.
-        Player& getPlayer();
-        const Card* getPreviousCard();
-        const Card* getCurrentCard();
-        void setCurrentCard( const Card*);
-
+        Game() {};
+        Game(CardDeck * cd): board(cd) {};
+        inline int getRound() const { return round; }; // returns a number between 0 and 6 corresponding to the current round of the game
+        void addPlayer( const std::string ); // which adds a Player to this game.
+        inline Player& getPlayer( Side s ) { return players[s]; } ;
+        inline const Card* getPreviousCard() const { return previousCard; };
+        inline const Card* getCurrentCard() const { return currentCard; };
+        void setCurrentCard( const Card* );
+        inline Card* getCard( const Letter& l, const Number& n ) { return board.getCard(l, n); }; // which calls the corresponding method in Board.
+        inline void setCard( const Letter& l, const Number& n, Card* c ) { board.setCard(l, n, c); }; // which calls the corresponding method in Board.
+        
         // A game must be printable with the insertion operator cout << game. It should display the board and all players.
+        friend std::ostream &operator<<(std::ostream & os, const Game & g);
 };
 
 #endif
--- src/game.cpp
+++ src/game.cpp
@@ -2,32 +2,36 @@
 #include <string>
 
 #include "game.h"
-// #include "player.h"
-// #include "card.h"
+#include "player.h"
+#include "card.h"
 
-int Game::getRound() {
-
-    return 0;
+// void Game::addPlayer( const Player& p ) {
+void Game::addPlayer( const std::string name) {
+    if (players.size() < 4) {
+        // players.insert(std::make_pair(p.getSide(), p));
+        players.insert(std::make_pair(Side(players.size()), Player(name, Side(players.size()))));
+    } else {
+        throw "Maximum amount of players reached!";
+    }
 }
 
-void Game::addPlayer( const Player& ) {
-
+void Game::setCurrentCard( const Card* c ) {
+    if (!previousCard) {
+        previousCard = currentCard;
+    } else {
+        previousCard = const_cast<Card*>(c);
+    }
+    currentCard = const_cast<Card*>(c);
 }
 
-// Player& Game::getPlayer() {
+std::ostream & operator<<(std::ostream & os, const Game & g) {
+    os << "=PLAYERS=" << std::endl;
+    for(auto player : g.players) {
+       os << player.second << std::endl;
+    }
+    os << std::endl << "=BOARD=" << std::endl;
+    Board b = g.board;
+    os << b;
 
-// }
-
-const Card* Game::getPreviousCard() {
-
-    return nullptr;
-}
-
-const Card* Game::getCurrentCard() {
-
-    return nullptr;
-}
-
-void Game::setCurrentCard( const Card*) {
-
+    return os;
 }
